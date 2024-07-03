82d09fd3c2a48291ee1c5e54eb059a23b8db63a2
Vladimir Guerrero <104907260+vladasblood@users.noreply.github.com>
2024-07-03 10:42:27 +0800

    Update ERC20Token.sol

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -1,1 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.3.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.3.0/contracts/access/Ownable.sol";
+
+contract VladimirToken is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("VAGToken", "VAGT") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        _transfer(_msgSender(), to, amount);
+        return true;
+    }
+}
